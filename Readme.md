dfb04b4b6561fdf4e73b347f292c66d8466ed31a
Stefan Edwards <saedwards.ecc@gmail.com>
2011-06-21 17:18:29 -0400

    initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+### ImageLite ###
+
+ A dead simple library for detecting mime type & dimensions of an image, with 0 dependencies outside of
+ Python, struct and StringIO. 
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='ImageLite',
+      version='0.1',
+      description='A lightweight image detection library',
+      author='Stefan Edwards, et al.',
+      author_email='saedwards.ecc@gmail.com',
+      url='http://github.com/saedwards-ecc/imagelite',
+      packages=['.'],
+     )
