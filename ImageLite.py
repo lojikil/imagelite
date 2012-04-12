111f7a0fe6bef94a3dc5e8ebcea84898e3f22962
Stefan Edwards <saedwards.ecc@gmail.com>
2012-04-12 11:42:09 -0400

    PEP8-afied ImageLite.py, added more information to Readme

--- ImageLite.py
+++ ImageLite.py
@@ -2,25 +2,28 @@ import struct
 import StringIO
 
 """
-    Dead simple image testing library; returns the mime type and x,y dimensions of the image in question.
+    Dead simple image testing library; returns the mime type and x,y
+    dimensions of the image in question.
     It is quite similar to:
     http://bfg-pages.googlecode.com/svn/trunk/pages/getimageinfo.py
-    A coworker pointed this code out after I had already completed the GIF, BMP & PNG portions, but it proved useful
-    since it was much better at finding JPEG dimensions than what I had written.
+    A coworker pointed this code out after I had already completed the GIF,
+    BMP & PNG portions, but it proved useful since it was much better at
+    finding JPEG dimensions than what I had written.
 
     The Bitmap header I used was from here:
         http://atlc.sourceforge.net/bmp.html
     The GIF information I used was from here:
         http://www.onicos.com/staff/iz/formats/gif.html
-    and the PNG information I used was from Wikipedia 
-        
+    and the PNG information I used was from Wikipedia
+
 """
 
+
 class ImageLite(object):
-    def __init__(self,data=None):
+    def __init__(self, data=None):
         self._gif_header0 = 'GIF87a'
         self._gif_header1 = 'GIF89a'
-        self._png_header  = '\x89PNG\r\n\x1a\n'
+        self._png_header = '\x89PNG\r\n\x1a\n'
         self._jpeg_header = 'jfif'
         self._exif_header = 'exif'
         self.width = 0
@@ -28,38 +31,44 @@ class ImageLite(object):
         self.image_type = ""
         if data != None:
             self.load(data)
-    def load(self,data):
+
+    def load(self, data):
         if data[:6] == self._gif_header0 or data[:6] == self._gif_header1:
             self.image_type = "image/gif"
-            self.width,self.height = struct.unpack("<HH",data[6:10])
+            self.width, self.height = struct.unpack("<HH", data[6:10])
         elif data[:8] == self._png_header:
             self.image_type = "image/png"
             offset = data.find("IHDR") + 4
-            self.width,self.height = struct.unpack(">LL",data[offset:offset+8])
+            self.width, self.height = struct.unpack(">LL",
+                            data[offset: offset + 8])
         elif data[:2] == "BM":
             self.image_type = "image/x-ms-bitmap"
-            self.width,self.height = struct.unpack("<ii",data[0x12:0x1a])
-        elif data[0] == '\xFF' and (data[6:10].lower() == self._jpeg_header or data[6:10].lower() == self._exif_header):
+            self.width, self.height = struct.unpack("<ii", data[0x12:0x1a])
+        elif data[0] == '\xFF' and (data[6:10].lower() == self._jpeg_header \
+                        or data[6:10].lower() == self._exif_header):
             self.image_type = "image/jpeg"
-            """ 
-                The code below is from 
+            """
+                The code below is from
                 http://bfg-pages.googlecode.com/svn/trunk/pages/getimageinfo.py
-                Originally I had something else, but this worked *much* better, and is New BSD licensed, so I
-                borrowed that. 
+                Originally I had something else, but this worked *much* better,
+                and is New BSD licensed, so I borrowed that.
             """
             jpeg = StringIO.StringIO(data)
             jpeg.read(2)
             b = jpeg.read(1)
             try:
                 while (b and ord(b) != 0xDA):
-                    while (ord(b) != 0xFF): b = jpeg.read(1)
-                    while (ord(b) == 0xFF): b = jpeg.read(1)
+                    while (ord(b) != 0xFF):
+                        b = jpeg.read(1)
+                    while (ord(b) == 0xFF):
+                        b = jpeg.read(1)
                     if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
                         jpeg.read(3)
                         h, w = struct.unpack(">HH", jpeg.read(4))
                         break
                     else:
-                        jpeg.read(int(struct.unpack(">H", jpeg.read(2))[0])-2)
+                        jpeg.read(int(struct.unpack(">H",
+                                                        jpeg.read(2))[0]) - 2)
                     b = jpeg.read(1)
                 self.width = int(w)
                 self.height = int(h)
@@ -70,5 +79,6 @@ class ImageLite(object):
 
     def __str__(self):
         if self.image_type != "":
-            return "{0} width: {1}, height: {2}".format(self.image_type,self.width,self.height)
+            return "{0} width: {1}, height: {2}".format(self.image_type,
+                            self.width, self.height)
         return "ImageLite"
--- Readme.md
+++ Readme.md
@@ -2,3 +2,22 @@
 
  A dead simple library for detecting mime type & dimensions of an image, with 0 dependencies outside of
  Python, struct and StringIO. 
+
+### Example Usage ###
+
+ImageLite was written because I had a web application that needed to detect type & dimension of 
+user-supplied images. To this end, it is meant to be a a drop-in replacement for using a more 
+full-featured library such as PIL (which is where the name comes from; originally we were using
+PIL's `Image` class to do much the same thing, but PIL is quite heavy-weight for something so simple).
+
+    from ImageLite import ImageLite
+    f = open('some_image','rb')
+    data = f.read()
+    img = ImageLite(data)
+    print img.width, img.height, img.image_type
+
+### To Do ###
+
+- More image types would be nice
+- Support for raw file handles, ala the JPEG work
+- &c.
